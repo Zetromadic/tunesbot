798f6ca30e8526d45ae6f458e27eba7d476da483
Zetromadic <58966456+Zetromadic@users.noreply.github.com>
2020-11-30 20:07:48 -0600

    TunesBot

--- index.js
+++ index.js
@@ -1,37 +1,153 @@
-require("dotenv").config();//Loading .env
-const fs = require("fs");
-const { Collection, Client } = require("discord.js");
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const client = new Client();//Making a discord bot client
-client.commands = new Collection();//Making client.commands as a Discord.js Collection
-client.queue = new Map()
+const token = process.env.token;
 
-client.config = {
-  prefix: process.env.PREFIX
-}
+const PREFIX = '-';
 
-//Loading Events
-fs.readdir(__dirname + "/events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach((file) => {
-    const event = require(__dirname + `/events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-    console.log("Loading Event: "+eventName)
-  });
-});
+const cheerio = require ('cheerio');
+const request = require('request');
+const ytdl = require('ytdl-core');
 
-//Loading Commands
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    client.commands.set(commandName, props);
-    console.log("Loading Command: "+commandName)
-  });
-});
+var version = '0.0.1';
 
-//Logging in to discord
-client.login(process.env.TOKEN)
+var servers = {};
+
+const fs = require('fs');
+var schedule = require('node-schedule');
+
+const ms = require('ms');
+var profanitites = require('profanities');
+
+client.on('ready', () => {
+    console.log('This bot is online!')
+    client.user.setActivity('-help | -play', { type: 'PLAYING'}).catch(console.error);
+})
+
+client.on('message', message=> {
+
+    if(!message.content.startsWith(PREFIX)) return;
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+
+    switch(args[0]){
+
+       
+        case 'info':
+            if(args[1] === 'version'){
+                message.delete();
+                message.channel.send('Version ' + version)
+            }
+            else if(args[1] === 'ping')
+            {
+
+                message.channel.send("Pinging...").then(m => {
+                    let ping = m.createdTimestamp - message.createdTimestamp
+                    
+                    m.edit(`Bot Latency: \`${ping}\``)
+                })
+
+                message.delete();
+            }
+            else if(args[1] === 'uptime')
+            {
+                message.delete();
+
+                function duration(ms)
+                {
+                    const sec = Math.floor((ms/1000) % 60).toString()
+                    const min = Math.floor((ms / (1000 * 60)) % 60).toString()
+                    const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
+                    const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
+                    return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} mintues, ${sec.padStart(2, '0')} seconds,`
+                }
+
+                message.channel.send(`Uptime: ${duration(client.uptime)}`)
+
+            }
+            else{
+                message.delete();
+                message.reply('Please follow with "uptime", "ping", or "version"')
+            }
+        break;
+
+        case 'help':
+        
+            message.delete();
+
+            let helpEmbed = new Discord.MessageEmbed()
+            .setTitle('Help')
+            .setColor('0x00FFC7')
+            .addField('-play', 'Followed by a song you would liked played while in a voice chat!')
+            .addField('-skip', 'This will skip the song that is playing!')
+            .addField('-stop', 'This will stop the queue of songs!')
+            .addField('-info', 'Followed by "Ping", "Uptime", or "Version"')
+
+            message.channel.send(helpEmbed);
+
+        break;
+
+        case 'play':
+
+            function play(connection, message){
+                var server = servers[message.guild.id];
+
+                server.dispatcher = connection.play(ytdl(server.queue[0], {filter: "audioonly"}));
+
+                server.queue.shift();
+
+                server.dispatcher.on("finish", function(){
+                    if(server.queue[0]){
+                        play(connection, message);
+                    }else{
+                        connection.disconnect();
+                    }
+                })
+            }
+        
+            if(!args[1])
+            {
+                message.channel.send("You need to provide a link to a song!");
+            }
+
+            if(!message.member.voice.channel){
+                message.channel.send("You must be in a voice channel to play a song!");
+                return;
+            }
+
+            if(!servers[message.guild.id]) servers[message.guild.id] = {
+                queue: []
+            }
+
+            var server = servers[message.guild.id];
+
+            server.queue.push(args[1]);
+
+            if(!message.guild.voiceConnection) message.member.voice.channel.join().then(function(connection){
+                play(connection, message);
+            })
+
+        break;
+
+        case 'skip':
+            var server = servers[message.guild.id];
+            if(server.dispatcher) server.dispatcher.end();
+        break;
+
+        case 'stop':
+            var server = servers[message.guild.id];
+            if(message.guild.voice.connection){
+                for(var i = server.queue.length -1; i >=0; i--){
+                    server.queue.splice(i, 1);
+                }
+                server.dispatcher.end;
+                console.log('stopped the queue');
+            }
+
+            if(message.guild.connection) message.guild.voice.connection.disconnect();
+        break;
+    }
+
+})
+
+client.login(token);
